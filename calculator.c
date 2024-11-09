4ae951fa6a813df1cdaf8fa1dcbf7c0c3032c248
voncoder1 <brandontakang3@gmail.com>
2024-11-08 17:08:03 -0800

    i have added a fibbonacci recursive function

--- calculator.c
+++ calculator.c
@@ -30,12 +30,33 @@ int squareroot(int n){
    return a;
 }
 
+// update to our code
+int fibbo(int n)
+{
+    if(n==1)
+    {
+        return 1;
+    }
+
+    else if(n==0)
+    {
+        return 0;
+    }
+
+    else
+    {
+        return fibbo(n-1)+fibbo(n-2);
+    }
+}
+
 
 int main(){
 
 printf("%d\n",facto(5));
 printf("%d\n",power(2,3));
 printf("%d\n",squareroot(5));
+//updated git code
+printf("%d\n",fibbo(13));
 
     return 0;
 }
